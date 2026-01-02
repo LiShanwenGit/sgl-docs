2f28b3c658a37c0643413097ea19d7bdcbbb12b6
lishanwen <1477153217@qq.com>
2026-01-02 20:41:12 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 templates_path = ['_templates']
