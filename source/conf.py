10e05d3a198f2e6de663c7fd6e7c2ae42f7d407c
lishanwen <1477153217@qq.com>
2026-01-02 20:11:42 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -9,8 +9,8 @@
 #import sphinx_rtd_theme
 
 project = 'SGL'
-copyright = '2024, li-shanwen'
-author = 'li-shanwen'
+copyright = '2026, SGL All contributors'
+author = 'SGL Team'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
@@ -29,8 +29,7 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "classic"
 html_static_path = ['_static']
 templates_path = ['_templates']
 exclude_patterns = []
