52bb4ec7c2f8178c283321005a8b2259b958b039
lishanwen <1477153217@qq.com>
2026-01-17 14:18:54 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -29,8 +29,6 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = [
-    '_static', 'docs/imgs'
-]
+html_static_path = ['_static']
 templates_path = ['_templates']
 exclude_patterns = []
